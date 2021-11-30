65a40beef7dd159bcb0258aea372eca6dfcf5f25
Jasoncbk998 <jasoncbk998@163.com>
2021-11-30 21:49:24 +0800

    新增若干curd and order and group

--- go_mysql/go2gorm/demo.go
+++ go_mysql/go2gorm/demo.go
@@ -12,16 +12,20 @@ import (
 	"log"
 )
 
+//连接实例
 func Conn(user, password, host, db, port string) *gorm.DB {
 	connArgs := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, port, db)
 	db_instance, err := gorm.Open("mysql", connArgs)
 	if err != nil {
 		log.Fatalf("连接mysql err:", err)
 	}
+	db_instance.SingularTable(true)
 	return db_instance
 }
 
+//表结构体
 type XzAutoServerConf struct {
+	Id         string `gorm:"column:id"`
 	GroupZone  string `gorm:"column:group_zone"`
 	ServerId   int    `gorm:"column:server_id"`
 	OpenTime   string `gorm:"coloumn:open_time"`
@@ -39,12 +43,58 @@ func main() {
 	db := Conn(user, password, host, db_name, port)
 	db.LogMode(true)
 	defer db.Close()
-	orm_op(db)
+	//insert_data(db)
+	groupby_data(db)
+
 }
 
-func orm_op(db *gorm.DB) {
-	//var rows []XzAutoServerConf
-	add_data := XzAutoServerConf{GroupZone: "20", ServerName: "jason", ServerId: 811}
-	db.Create(&add_data)
+func insert_data(db *gorm.DB) {
+	add_data := XzAutoServerConf{GroupZone: "26", ServerId: 10, ServerName: "abc", Username: "aaaaaaa", Status: 26}
+	create := db.Create(&add_data)
+	if create.Error != nil {
+		log.Fatal(create.Error)
+	}
+}
 
+var rows []XzAutoServerConf
+
+func delete_data(db *gorm.DB) {
+
+	err := db.Model(&rows).Where("group_zone=?", "21").Delete(&XzAutoServerConf{}).Error
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func search_data(db *gorm.DB) {
+	db.Table("xz_auto_server_conf").Where("status=?", "22").Select([]string{"group_zone", "server_id", "open_time", "server_name", "username"}).Find(&rows)
+	for _, v := range rows {
+		fmt.Println(v.Username)
+	}
+}
+
+func order_data(db *gorm.DB) {
+	db.Table("xz_auto_server_conf").Where("status>=?", 23).Select([]string{"username", "id"}).Order("status desc").Find(&rows)
+	for _, v := range rows {
+		fmt.Println(v.Id)
+	}
+}
+
+//封装对象必须大小写
+type Result struct {
+	Group_Zone string
+	Num_        int64
+}
+
+func groupby_data(db *gorm.DB) {
+	var results []Result
+	db.Table("xz_auto_server_conf").Select("group_zone,count(*) as num_").Group("group_zone").Scan(&results)
+	fmt.Println(results)
+}
+
+func update_data(db *gorm.DB) {
+	err := db.Model(&rows).Where("status=?", 26).Update("group_zone", 26).Error
+	if err != nil {
+		log.Fatal(err)
+	}
 }
